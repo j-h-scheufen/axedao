a8b0ddbfaf6ca61b196651c4ef90766879afbce0
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-01-10 14:26:25 -0500

    Added test for LP provision and swapping.
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- test/UNIv2SwapTax.ts
+++ test/UNIv2SwapTax.ts
@@ -5,10 +5,12 @@ const pairJson = require("@uniswap/v2-periphery/build/IUniswapV2Pair.json");
 import { Address } from "@nomicfoundation/ethereumjs-util";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
-import { AddressLike } from "ethers";
 import { ethers } from "hardhat";
 
 const WETH9 = require("./WETH9.json");
+const DEADLINE = Math.floor(Date.now() / 1000) + 10 * 60; // 10 min
+const AXE_SUPPLY = ethers.parseUnits("5000000000");
+const DAI_SUPPLY = ethers.parseUnits("1000");
 
 describe("Uniswap Tests", function () {
 
@@ -28,9 +30,9 @@ describe("Uniswap Tests", function () {
     const dai = await DAI.deploy();
     console.log('DAI deployed to', dai.target);
 
-    // Mint some initial supply in both tokens for the owner
-    await axe.issue(ethers.parseUnits("1000000"));
-    await dai.mint(owner, ethers.parseUnits("1000"));
+    // Mint initial supply in both tokens for the owner
+    await axe.issue(AXE_SUPPLY);
+    await dai.mint(owner, DAI_SUPPLY);
 
     const tx1 = await factory.createPair(axe.target, dai.target);
     await tx1.wait();
@@ -62,8 +64,8 @@ describe("Uniswap Tests", function () {
   describe("Deployment", function () {
     it("Should have a pool deployed with no liquidity", async function () {
       const { owner, router, pair, axe, dai } = await loadFixture(deployUniswapFixture);
-      expect(await axe.totalSupply()).to.equal(ethers.parseUnits("1000000"));
-      expect(await dai.totalSupply()).to.equal(ethers.parseUnits("1000"));
+      expect(await axe.totalSupply()).to.equal(AXE_SUPPLY);
+      expect(await dai.totalSupply()).to.equal(DAI_SUPPLY);
       const reserves = await pair.getReserves();
       expect(reserves[0]).to.be.equal(0, 'Token1 pool liquidity should be 0.');
       expect(reserves[1]).to.be.equal(0, 'Token2 pool liquidity should be 0.');
@@ -72,42 +74,69 @@ describe("Uniswap Tests", function () {
     });
   });
 
-  describe("Liquidity & Trading", function () {
+  describe("Liquidity", function () {
     it("Should be able to add liquidity", async function () {
       const { owner, router, pair, axe, dai } = await loadFixture(deployUniswapFixture);
-
       const axeAmount = ethers.parseUnits("100000");
       const daiAmount = ethers.parseUnits("10");
 
-      const lpTokenBalanceBefore = await pair.balanceOf(owner.address);
-      console.log(
-        `LP tokens for the owner before: ${lpTokenBalanceBefore.toString()}`
-      );
+      expect( await pair.balanceOf(owner.address)).to.be.equal(0, "Owner should have no liquidity")
+      await addLiquidity(owner, router, axe, dai, axeAmount, daiAmount);
 
-      const deadline = Math.floor(Date.now() / 1000) + 10 * 60; // 10 min
-      // const addLiquidityTx = await router
-      //   .connect(owner)
-      //   .addLiquidity(
-      //     axe,
-      //     dai,
-      //     axeAmount,
-      //     daiAmount,
-      //     0,
-      //     0,
-      //     owner,
-      //     deadline
-      //   );
-      // await addLiquidityTx.wait();
-
-      // Check LP token balance for the owner
-      const lpTokenBalance = await pair.balanceOf(owner.address);
-      console.log(`LP tokens for the owner after: ${lpTokenBalance.toString()}`);
-
+      expect( await pair.balanceOf(owner.address)).to.be.greaterThan(0, "Owner should have LP tokens")
       const reserves = await pair.getReserves();
-      console.log(`Reserves: ${reserves[0].toString()}, ${reserves[1].toString()}`);
+      expect(reserves[0]).to.be.equal(daiAmount, 'DAI pool liquidity mismatch.');
+      expect(reserves[1]).to.be.equal(axeAmount, 'AXE pool liquidity mismatch.');
 
     });
+    it("Should be able to swap", async function () {
+      const { owner, router, pair, axe, dai } = await loadFixture(deployUniswapFixture);
+      const axeAmount = ethers.parseUnits("100000000");
+      const daiAmount = ethers.parseUnits("500");
+      await addLiquidity(owner, router, axe, dai, axeAmount, daiAmount);
+
+      let reservesBefore = await pair.getReserves();
+      await swapTokens(owner, router, dai, axe, ethers.parseUnits("4"))
+      let reservesAfter = await pair.getReserves();
+      expect(reservesBefore[0]).to.be.lessThan(reservesAfter[0]);
+      expect(reservesBefore[1]).to.be.greaterThan(reservesAfter[1]);
+
+      reservesBefore = reservesAfter;
+      await swapTokens(owner, router, axe, dai, ethers.parseUnits("872347"))
+      reservesAfter = await pair.getReserves();
+      expect(reservesBefore[0]).to.be.greaterThan(reservesAfter[0]);
+      expect(reservesBefore[1]).to.be.lessThan(reservesAfter[1]);
+    });
+
   });
 
-
 });
+
+const addLiquidity = async (owner: any, router: any, axe: any, dai: any, axeAmount: bigint, daiAmount: bigint): Promise<void> => {
+  const addLiquidityTx = await router
+    .connect(owner)
+    .addLiquidity(
+      axe,
+      dai,
+      axeAmount,
+      daiAmount,
+      0,
+      0,
+      owner,
+      DEADLINE
+    );
+  await addLiquidityTx.wait();
+}
+
+const swapTokens = async (owner: any, router: any, tokenIn: any, tokenOut: any, amount: bigint ): Promise<void> => {
+  const addLiquidityTx = await router
+    .connect(owner)
+    .swapExactTokensForTokens(
+      amount,
+      0,
+      [tokenIn, tokenOut],
+      owner,
+      DEADLINE
+    );
+  await addLiquidityTx.wait();
+}
