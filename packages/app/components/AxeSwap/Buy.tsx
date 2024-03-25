dbfdf3211ce7cb2795c38464d48d32095d1d7a56
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-03-24 23:41:37 -0400

    Implemented allowance and balance checks and approval tx in Buy.tsx
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- packages/app/components/AxeSwap/Buy.tsx
+++ packages/app/components/AxeSwap/Buy.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { debounce } from 'lodash';
-import { parseUnits, formatUnits } from 'viem';
+import { parseUnits, formatUnits, Address } from 'viem';
 import { useAccount, useWaitForTransactionReceipt } from 'wagmi';
 import { Button } from '@nextui-org/button';
 import { Input } from '@nextui-org/input';
@@ -10,8 +10,10 @@ import { Input } from '@nextui-org/input';
 import ENV from '@/config/environment';
 import {
   useReadIUniswapV2Router02GetAmountsOut,
-  useWriteIUniswapV2Router01SwapExactTokensForTokens,
+  useReadErc20Allowance,
   useReadAxeBuyTax,
+  useWriteErc20Approve,
+  useWriteIUniswapV2Router01SwapExactTokensForTokens,
 } from '@/generated';
 import { formatAxeUnits, formatStableUnits } from '@/utils/contract.utils';
 import { TradeFormProps } from './AxeSwap';
@@ -27,17 +29,30 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
   const account = useAccount();
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const [exceedsAllowance, setExceedsAllowance] = useState<boolean>(false);
+  const [exceedsBalance, setExceedsBalance] = useState<boolean>(false);
   const [usdInput, setUsdInput] = useState<string>('');
   const [axeOutput, setAxeOutput] = useState<AxeOutput>({ axeReceived: 0, axeDonated: 0 });
   const [showOutput, setShowOutput] = useState<boolean>(false);
+
+  /// WAGMI
   const { data: buyTax } = useReadAxeBuyTax({ address: ENV.axeTokenAddress });
+  const { data: allowance, refetch: updateAllowance } = useReadErc20Allowance({
+    address: ENV.axeSwapTokenAddress,
+    args: [account.address as Address, ENV.uniswapV2RouterAddress],
+  });
   const { data: amountOut } = useReadIUniswapV2Router02GetAmountsOut({
     address: ENV.uniswapV2RouterAddress,
     args: [parseUnits(usdInput, 18), [ENV.axeSwapTokenAddress, ENV.axeTokenAddress]],
   });
-
-  const { data: hash, isPending, writeContract: swap } = useWriteIUniswapV2Router01SwapExactTokensForTokens();
-  const { isLoading, isSuccess, error: swapError } = useWaitForTransactionReceipt({ hash });
+  const {
+    data: swapHash,
+    isPending: swapPending,
+    writeContract: swap,
+  } = useWriteIUniswapV2Router01SwapExactTokensForTokens();
+  const { isSuccess: swapSuccess, error: swapError } = useWaitForTransactionReceipt({ hash: swapHash });
+  const { data: approveHash, isPending: approvePending, writeContract: approve } = useWriteErc20Approve();
+  const { isSuccess: approveSuccess, error: approveError } = useWaitForTransactionReceipt({ hash: approveHash });
 
   useEffect(() => {
     if (amountOut && amountOut.length > 1 && amountOut[1] > 0) {
@@ -51,29 +66,51 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
   }, [amountOut, buyTax]);
 
   useEffect(() => {
-    onUpdate();
-  }, [isSuccess, onUpdate]);
+    if (swapSuccess) onUpdate();
+  }, [swapSuccess, onUpdate]);
 
-  const updateUsdInput = debounce((value: string) => {
+  useEffect(() => {
+    if (approveSuccess) updateAllowance();
+  }, [approveSuccess, updateAllowance]);
+
+  useEffect(() => {
+    if (allowance && allowance < parseUnits(usdInput, 18)) {
+      setExceedsAllowance(true);
+    } else setExceedsAllowance(false);
+  }, [usdInput, allowance]);
+
+  // TODO use simulate contract to prepare and only then enable the Buy / Approve button
+  // TODO calculate price impact to show % price move and warn (https://stackoverflow.com/questions/74282316/uniswap-v2-pool-priceimpact#:~:text=You%20can%20calculate%20the%20price,is%20ETH%2C%20k%20is%202%2C000%2C000%2C000.)
+
+  // debouncing the usd input
+  const updateInput = debounce((value: string) => {
     setUsdInput(value);
+    if (swapBalance && swapBalance < parseUnits(value, 18)) {
+      setExceedsBalance(true);
+    } else setExceedsBalance(false);
     setIsUpdating(false);
   }, 1000);
 
   const submit = () => {
     try {
       setIsSubmitting(true);
-      const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
-      //TODO check for slippage and warn
-      swap({
-        address: ENV.uniswapV2RouterAddress,
-        args: [
-          parseUnits(usdInput, 18),
-          parseUnits((axeOutput.axeReceived - axeOutput.axeReceived / slippageTolerance).toString(), 18), //
-          [ENV.axeSwapTokenAddress, ENV.axeTokenAddress],
-          account.address!,
-          BigInt(deadline),
-        ],
-      });
+      const bigInput = parseUnits(usdInput, 18);
+      if (exceedsAllowance) {
+        approve({ address: ENV.axeSwapTokenAddress, args: [ENV.uniswapV2RouterAddress, bigInput] });
+      } else {
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
+        //TODO check for slippage and warn / adjust tolerance or block
+        swap({
+          address: ENV.uniswapV2RouterAddress,
+          args: [
+            parseUnits(usdInput, 18),
+            parseUnits((axeOutput.axeReceived - axeOutput.axeReceived / slippageTolerance).toString(), 18),
+            [ENV.axeSwapTokenAddress, ENV.axeTokenAddress],
+            account.address!,
+            BigInt(deadline),
+          ],
+        });
+      }
     } catch (error) {
       console.error('Error during swap.', error);
       throw error;
@@ -90,6 +127,8 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
         type="number"
         placeholder="0.00"
         size="lg"
+        isInvalid={exceedsBalance}
+        errorMessage={exceedsBalance ? 'Input exceeds balance' : undefined}
         classNames={{ label: 'py-1', input: 'text-large number-to-text' }}
         startContent={
           <div className="pointer-events-none flex items-center">
@@ -98,7 +137,7 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
         }
         onValueChange={(value: string) => {
           setIsUpdating(true);
-          updateUsdInput(value);
+          updateInput(value);
         }}
       />
       <div className="my-1">1 Axé &#8776; ${reserves.rate}</div>
@@ -132,17 +171,17 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
 
       <Button
         className="mt-2 size-unit-lg w-full bg-green-500 text-large dark:bg-green-700"
-        disabled={isPending || isUpdating}
+        isDisabled={swapPending || approvePending || isUpdating || exceedsBalance}
         isLoading={isSubmitting}
         onClick={submit}
       >
-        {isPending ? 'Confirming...' : isUpdating ? 'Updating ...' : 'Buy'}
+        {swapPending ? 'Confirming...' : isUpdating ? 'Updating ...' : exceedsAllowance ? 'Approve' : 'Buy'}
       </Button>
       <div>
-        {hash && <div className="mt-4">TX Receipt: {hash}</div>}
-        {isLoading && <div className="mt-4">Waiting for confirmation...</div>}
-        {isSuccess && <div className="mt-4">Transaction confirmed.</div>}
-        {swapError && <div className="mt-4">Transaction failed: {swapError.message}</div>}
+        {approveSuccess && <div className="mt-4">Approval confirmed.</div>}
+        {approveError && <div className="mt-4">Approval failed: {approveError.message}</div>}
+        {swapSuccess && <div className="mt-4">Swap confirmed.</div>}
+        {swapError && <div className="mt-4">Swap failed: {swapError.message}</div>}
       </div>
     </div>
   );
