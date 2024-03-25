5da5266d1c4c8ee79e8e385fc1c60f371e838fb0
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-03-25 16:46:26 -0400

    Added formik dependency and used useFormik in Buy.tsx for better state management and validation
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- packages/app/app/_providers/snackbar.provider.tsx
+++ packages/app/app/_providers/snackbar.provider.tsx
@@ -4,5 +4,9 @@ import { PropsWithChildren } from 'react';
 import { SnackbarProvider } from 'notistack';
 
 export function Provider({ children }: PropsWithChildren) {
-  return <SnackbarProvider autoHideDuration={5000}>{children}</SnackbarProvider>;
+  return (
+    <SnackbarProvider autoHideDuration={5000} anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+      {children}
+    </SnackbarProvider>
+  );
 }
--- packages/app/components/AxeSwap/Buy.tsx
+++ packages/app/components/AxeSwap/Buy.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { debounce } from 'lodash';
 import { parseUnits, formatUnits, Address } from 'viem';
 import { useAccount, useWaitForTransactionReceipt } from 'wagmi';
 import { Button } from '@nextui-org/button';
 import { Input } from '@nextui-org/input';
 import { enqueueSnackbar, closeSnackbar, SnackbarKey } from 'notistack';
+import { useFormik, FormikErrors, FormikHelpers } from 'formik';
 
 import ENV from '@/config/environment';
 import {
@@ -28,10 +29,8 @@ const slippageTolerance = 100; //basispoints
 
 const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUpdate }) => {
   const account = useAccount();
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
   const [exceedsAllowance, setExceedsAllowance] = useState<boolean>(false);
-  const [exceedsBalance, setExceedsBalance] = useState<boolean>(false);
   const [showOutput, setShowOutput] = useState<boolean>(false);
   const [usdInput, setUsdInput] = useState<string>('');
   const [axeOutput, setAxeOutput] = useState<AxeOutput>({ axeReceived: 0, axeDonated: 0 });
@@ -64,7 +63,7 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
     isLoading: approveLoading,
   } = useWaitForTransactionReceipt({ hash: approveHash });
 
-  // apply buyTax on getAmountsOut estimate as the Axé is taken from the swap output
+  // apply buyTax on getAmountsOut estimate as the Axé donation is taken from the swap output
   useEffect(() => {
     if (amountOut && amountOut > 0n) {
       const axeOut = Number(formatUnits(amountOut, 18));
@@ -125,19 +124,9 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
   // TODO use simulate contract to prepare and only then enable the Buy / Approve button
   // TODO calculate price impact to show % price move and warn (https://stackoverflow.com/questions/74282316/uniswap-v2-pool-priceimpact#:~:text=You%20can%20calculate%20the%20price,is%20ETH%2C%20k%20is%202%2C000%2C000%2C000.)
 
-  // debouncing the usd input
-  const updateInput = debounce((value: string) => {
-    setUsdInput(value);
-    if (swapBalance && swapBalance < parseUnits(value, 18)) {
-      setExceedsBalance(true);
-    } else setExceedsBalance(false);
-    setIsUpdating(false);
-  }, 1000);
-
-  const submit = () => {
+  const handleSubmit = (values: FormValues, { setSubmitting }: FormikHelpers<FormValues>) => {
     try {
-      setIsSubmitting(true);
-      const bigInput = parseUnits(usdInput, 18);
+      const bigInput = parseUnits(values.amountIn.toString(), 18);
       if (exceedsAllowance) {
         approve({ address: ENV.axeSwapTokenAddress, args: [ENV.uniswapV2RouterAddress, bigInput] });
       } else {
@@ -153,74 +142,111 @@ const Buy: React.FC<TradeFormProps> = ({ reserves, swapBalance, axeBalance, onUp
             BigInt(deadline),
           ],
         });
+        formik.resetForm();
       }
     } catch (error) {
       console.error('Error during swap.', error);
       throw error;
     } finally {
-      setIsSubmitting(false);
+      setSubmitting(false);
     }
   };
 
-  return (
-    <div className="flex w-auto flex-col items-center gap-2">
-      <div>Your Balance: ${formatStableUnits(swapBalance)}</div>
-      <Input
-        label="You pay"
-        type="number"
-        placeholder="0.00"
-        size="lg"
-        isInvalid={exceedsBalance}
-        errorMessage={exceedsBalance ? 'Input exceeds balance' : undefined}
-        classNames={{ label: 'py-1', input: 'text-large number-to-text' }}
-        startContent={
-          <div className="pointer-events-none flex items-center">
-            <span className="text-large text-default-400">$</span>
-          </div>
-        }
-        onValueChange={(value: string) => {
-          setIsUpdating(true);
-          updateInput(value);
-        }}
-      />
-      <div className="my-1">1 Axé &#8776; ${reserves.rate}</div>
-      <Input
-        label="You receive"
-        type="number"
-        size="lg"
-        disabled
-        classNames={{ label: 'py-1', input: 'text-large' }}
-        startContent={
-          <div className="pointer-events-none flex items-center">
-            <span className="text-large text-default-400">Axé</span>
-          </div>
-        }
-        value={axeOutput.axeReceived > 0 ? axeOutput.axeReceived.toFixed(4).toLocaleString() : '0'}
-        description={
-          showOutput ? (
-            <span>
-              You are donating {Number(buyTax) / 100}% (
-              {axeOutput.axeDonated < 1 ? '< 1' : Math.trunc(axeOutput.axeDonated)} Axé) to the{' '}
-              <a href="https://axe.to.hns" className=" underline">
-                Axé DAO
-              </a>
-            </span>
-          ) : (
-            ''
-          )
-        }
-      />
-      <div>Your Balance: {formatAxeUnits(axeBalance)} Axé</div>
+  interface FormValues {
+    amountIn: string;
+  }
 
-      <Button
-        className="mt-2 size-unit-lg w-full bg-green-500 text-large dark:bg-green-700"
-        isDisabled={swapPending || approvePending || isUpdating || exceedsBalance}
-        isLoading={isSubmitting}
-        onClick={submit}
-      >
-        {swapPending ? 'Confirming...' : isUpdating ? 'Updating ...' : exceedsAllowance ? 'Approve' : 'Buy'}
-      </Button>
-    </div>
+  const validateForm = useCallback(
+    async (values: FormValues): Promise<FormikErrors<FormValues>> => {
+      const errors: FormikErrors<FormValues> = {};
+      if (!!values.amountIn) {
+        const bigAmount = parseUnits(values.amountIn.toString(), 18); // Note: explicit cast toString() required here!
+        if (swapBalance && swapBalance < bigAmount) errors.amountIn = 'Available balance exceeded';
+      }
+      return errors;
+    },
+    [swapBalance],
+  );
+
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      amountIn: '',
+    },
+    onSubmit: handleSubmit,
+    validate: validateForm,
+  });
+
+  // debounce the input that leads to recalculations
+  const handleChange = debounce((value: string) => {
+    setUsdInput(value);
+    setIsUpdating(false);
+  }, 1000);
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className="flex w-auto flex-col items-center gap-2">
+        <div>Your Balance: ${formatStableUnits(swapBalance)}</div>
+        <Input
+          name="amountIn"
+          label="You pay"
+          type="number"
+          placeholder="0.00"
+          size="lg"
+          classNames={{ label: 'py-1', input: 'text-large number-to-text' }}
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-large text-default-400">$</span>
+            </div>
+          }
+          value={formik.values.amountIn}
+          onChange={(event) => {
+            setIsUpdating(true);
+            handleChange(event?.target.value);
+            formik.handleChange(event);
+          }}
+          onBlur={formik.handleBlur}
+          isInvalid={formik.touched.amountIn && !!formik.errors.amountIn}
+          errorMessage={formik.errors.amountIn}
+        />
+        <div className="my-1">1 Axé &#8776; ${reserves.rate}</div>
+        <Input
+          label="You receive"
+          type="number"
+          size="lg"
+          disabled
+          classNames={{ label: 'py-1', input: 'text-large' }}
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-large text-default-400">Axé</span>
+            </div>
+          }
+          value={axeOutput.axeReceived > 0 ? axeOutput.axeReceived.toFixed(4).toLocaleString() : '0'}
+          description={
+            showOutput ? (
+              <span>
+                You are donating {Number(buyTax) / 100}% (
+                {axeOutput.axeDonated < 1 ? '< 1' : Math.trunc(axeOutput.axeDonated)} Axé) to the{' '}
+                <a href="https://axe.to.hns" className=" underline">
+                  Axé DAO
+                </a>
+              </span>
+            ) : (
+              ''
+            )
+          }
+        />
+        <div>Your Balance: {formatAxeUnits(axeBalance)} Axé</div>
+
+        <Button
+          type="submit"
+          className="mt-2 size-unit-lg w-full bg-green-500 text-large dark:bg-green-700"
+          isDisabled={!formik.isValid || !formik.dirty || isUpdating || approvePending || swapPending}
+          isLoading={formik.isSubmitting}
+        >
+          {swapPending ? 'Confirming...' : isUpdating ? 'Updating ...' : exceedsAllowance ? 'Approve' : 'Buy'}
+        </Button>
+      </div>
+    </form>
   );
 };
 
