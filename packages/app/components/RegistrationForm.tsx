78ae8700627df1de554a06674b72f1ba50e7d770
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-08-27 08:45:39 -0400

    Refactored RegistrationForm to extract a re-usable component TextInput. Fixed hydration errors on page reload by creating initialValues outside JSX code.
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- packages/app/components/RegistrationForm.tsx
+++ packages/app/components/RegistrationForm.tsx
@@ -3,7 +3,7 @@
 import { Button } from '@nextui-org/button';
 import { Input } from '@nextui-org/input';
 import { AxiosError } from 'axios';
-import { Field, FieldInputProps, FieldMetaProps, Form, Formik, FormikHelpers, FormikProps } from 'formik';
+import { Field, FieldProps, Form, Formik, FormikHelpers, FormikProps, useField } from 'formik';
 import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import { useAccount, useConnect } from 'wagmi';
@@ -11,6 +11,24 @@ import { injected } from 'wagmi/connectors';
 
 import { registrationFormSchema, RegistrationFormType } from '@/constants/schemas';
 import useRegister from '@/hooks/useRegister';
+
+/**
+ * A simple text-based input field for Formik Field components.
+ */
+const TextInput = (props: FieldProps['field']) => {
+  const [field, meta] = useField(props);
+  return (
+    <Input
+      {...field}
+      className="w-full"
+      classNames={{ inputWrapper: '!min-h-14', errorMessage: 'text-left' }}
+      color={meta.touched && meta.error ? 'danger' : undefined}
+      isInvalid={meta.touched && !!meta.error}
+      errorMessage={meta.touched && meta.error ? meta.error : undefined}
+      {...props}
+    />
+  );
+};
 
 const RegistrationForm = () => {
   const session = useSession();
@@ -23,83 +41,47 @@ const RegistrationForm = () => {
   const FormikForm = ({ setFieldValue, dirty, isValid }: FormikProps<RegistrationFormType>) => {
     const { address, isConnected } = useAccount();
     useEffect(() => {
-      setFieldValue('walletAddress', address || '');
+      if (isConnected && address) setFieldValue('walletAddress', address);
     }, [address, isConnected, setFieldValue]);
 
-          return (
+    return (
       <Form className="m-auto flex h-fit w-full max-w-sm flex-col gap-3">
-        <Field name="name">
-          {({ field, meta }: { field: FieldInputProps<string>; meta: FieldMetaProps<string> }) => (
-            <Input
-              {...field}
-              label="Nickname"
-              className="w-full"
-              classNames={{ inputWrapper: '!min-h-14', errorMessage: 'text-left' }}
-              color={meta.touched && meta.error ? 'danger' : undefined}
-              isInvalid={!!meta.error}
-              errorMessage={meta.touched && meta.error ? meta.error : undefined}
-            />
-          )}
-        </Field>
-        <Field name="email">
-          {({ field, meta }: { field: FieldInputProps<string>; meta: FieldMetaProps<string> }) => (
-            <Input
-              {...field}
-              type="email"
-              label="Email"
-              className="w-full"
-              classNames={{ inputWrapper: '!min-h-14', errorMessage: 'text-left' }}
-              color={meta.touched && meta.error ? 'danger' : undefined}
-              isInvalid={!!meta.error}
-              errorMessage={meta.touched && meta.error ? meta.error : undefined}
-            />
-          )}
-        </Field>
-
-      <div className="flex flex-col items-end">
-          <Field name="walletAddress">
-            {({ field, meta }: { field: FieldInputProps<string>; meta: FieldMetaProps<string> }) => (
-              <Input
-                {...field}
-                label="Wallet address (MetaMask)"
-                className="w-full"
-                classNames={{
-                  inputWrapper: '!min-h-14 data-[hover=true]:bg-initial',
-                  input: 'text-sm !text-default-500',
-                  errorMessage: 'text-left',
-                }}
-                color={meta.touched && meta.error ? 'danger' : undefined}
-                isInvalid={!!meta.error}
-                errorMessage={meta.touched && meta.error ? meta.error : undefined}
-                placeholder={!meta.value ? 'No wallet connected' : undefined}
-                readOnly
-              />
-            )}
-          </Field>
+        <Field name="name" label="Name" as={TextInput} />
+        <Field name="email" label="Email" type="email" as={TextInput} />
+        <Field
+          name="walletAddress"
+          label="Wallet address (MetaMask)"
+          as={TextInput}
+          classNames={{
+            inputWrapper: '!min-h-14 data-[hover=true]:bg-initial',
+            input: 'text-sm !text-default-500',
+            errorMessage: 'text-left',
+          }}
+          readOnly
+        />
         <Button
           size="sm"
           variant="ghost"
           className="ml-auto w-fit mt-2"
           onPress={() => connect({ connector: injected() })}
         >
-            {address ? 'Change' : 'Connect'}
+          {address ? 'Change' : 'Connect'}
         </Button>
-      </div>
-      {submitError && (
-        <div className="my-2 text-center text-small text-danger">
-          {(submitError as AxiosError<{ error: string }>).response?.data?.error || submitError.message}
-        </div>
-      )}
-      <Button
-        key="register-button"
-        type="submit"
-        color="primary"
-        className="mt-5 w-full"
-        isLoading={isSubmitting}
+        {submitError && (
+          <div className="my-2 text-center text-small text-danger">
+            {(submitError as AxiosError<{ error: string }>).response?.data?.error || submitError.message}
+          </div>
+        )}
+        <Button
+          key="register-button"
+          type="submit"
+          color="primary"
+          className="mt-5 w-full"
+          isLoading={isSubmitting}
           disabled={isLoading || isSubmitting || !dirty || !isValid}
-      >
-        Register
-      </Button>
+        >
+          Register
+        </Button>
       </Form>
     );
   };
@@ -115,13 +97,16 @@ const RegistrationForm = () => {
     }
   };
 
+  // NOTE: The initial form values MUST BE declared outside of the JSX code, otherwise it can lead to hydration errors.
+  const initValues: RegistrationFormType = {
+    name: '',
+    email: '',
+    walletAddress: '',
+  };
+
   return (
     <Formik<RegistrationFormType>
-      initialValues={{
-        name: '',
-        email: '',
-        walletAddress: '',
-      }}
+      initialValues={initValues}
       onSubmit={handleSubmit}
       validationSchema={registrationFormSchema}
     >
