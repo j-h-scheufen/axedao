15714801bf7b39821b33cc29322c78a899bccfda
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-10-03 12:43:35 -0400

    Added female options to titles enum in DB and constants.ts
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

---
+++ packages/app/db/migrations/0009_narrow_pepper_potts.sql
@@ -0,0 +1,7 @@
+ALTER TYPE "title" ADD VALUE 'mestra';--> statement-breakpoint
+ALTER TYPE "title" ADD VALUE 'contra-mestra';--> statement-breakpoint
+ALTER TYPE "title" ADD VALUE 'mestranda';--> statement-breakpoint
+ALTER TYPE "title" ADD VALUE 'professora';--> statement-breakpoint
+ALTER TYPE "title" ADD VALUE 'instrutora';--> statement-breakpoint
+ALTER TYPE "title" ADD VALUE 'monitora';--> statement-breakpoint
+ALTER TYPE "title" ADD VALUE 'aluna';
