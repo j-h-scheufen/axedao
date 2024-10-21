4ad0375e99a363917bdcb553dcd0e0cfbcaeb022
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-10-21 13:25:23 -0400

    Fixed missing CreateGroupAssociation on Profile
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- packages/quilombo/components/Profile/ProfileGroupAssociation.tsx
+++ packages/quilombo/components/Profile/ProfileGroupAssociation.tsx
@@ -5,17 +5,14 @@ import { useAtomValue } from 'jotai';
 
 import GroupCard from '@/components/GroupCard';
 import { currentUserGroupAtom } from '@/hooks/state/currentUser';
+import CreateGroupAssociation from '../CreateGroupAssociation';
 
 const ProfileGroupAssociation = () => {
   const { data: group, isFetching } = useAtomValue(currentUserGroupAtom);
 
   if (isFetching) return <Spinner />;
 
-  return group ? (
-    <GroupCard group={group} />
-  ) : (
-    <div className="text-default-500 text-sm">You are currently not associated with a group</div>
-  );
+  return group ? <GroupCard group={group} /> : <CreateGroupAssociation />;
 };
 
 export default ProfileGroupAssociation;
