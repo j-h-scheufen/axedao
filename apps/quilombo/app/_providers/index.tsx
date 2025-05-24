d91ad1a39b4b9aea1dc88b1c8b6e44ed07f4d0cd
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2025-05-24 08:56:41 -0400

    fix: missing client context in session.provider.tsx
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- apps/quilombo/app/_providers/index.tsx
+++ apps/quilombo/app/_providers/index.tsx
@@ -1,13 +1,18 @@
+import { getServerSession } from 'next-auth';
+
 import SessionProvider from './session.provider';
 import UiProvider from './ui.provider';
 import QueryProvider from './query.provider';
 import SnackbarProvider from './snackbar.provider';
 import Web3Provider from './wagmi.provider';
 import StateProvider from './jotai.provider';
+import { nextAuthOptions } from '@/config/next-auth-options';
 
-export default async function Providers({ children }: { children: React.ReactNode }) {
+const Providers = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession(nextAuthOptions);
+
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <UiProvider
         themeProps={{
           attribute: 'class',
@@ -16,8 +21,8 @@ export default async function Providers({ children }: { children: React.ReactNod
         }}
       >
         <SnackbarProvider>
+          {/* Note: Web3Provider and StateProvider rely on @tanstack/react-query. */}
           <QueryProvider>
-            {/* Note: Web3Provider and StateProvider rely on @tanstack/react-query. */}
             <Web3Provider>
               <StateProvider>{children}</StateProvider>
             </Web3Provider>
@@ -26,4 +31,6 @@ export default async function Providers({ children }: { children: React.ReactNod
       </UiProvider>
     </SessionProvider>
   );
-}
+};
+
+export default Providers;
