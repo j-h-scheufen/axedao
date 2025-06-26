5b9b14536299dd015b4c6b9fb6cc2d251abafbe6
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2025-06-27 00:16:20 +0200

    chore: added styleUrl, initialCenter, and initialZoom props to BaseMapLibreMap
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- apps/quilombo/components/geocode/GroupLocationsMap.tsx
+++ apps/quilombo/components/geocode/GroupLocationsMap.tsx
@@ -2,12 +2,17 @@
 
 import type { FeatureCollection, Geometry } from 'geojson';
 import { useAtomValue } from 'jotai';
+
 import { locationsAtom } from '@/hooks/state/location';
 import BaseMapLibreMap from './BaseMapLibreMap';
 
 interface GroupLocationsMapProps {
   geojsonData?: FeatureCollection<Geometry>;
 }
+
+const SATELLITE_STYLE_URL = 'https://api.maptiler.com/maps/satellite/style.json';
+const INITIAL_CENTER: [number, number] = [0, 20];
+const INITIAL_ZOOM = 1.2;
 
 const GroupLocationsMap = ({ geojsonData }: GroupLocationsMapProps) => {
   const { data: locations, isPending, error } = useAtomValue(locationsAtom);
@@ -40,7 +45,14 @@ const GroupLocationsMap = ({ geojsonData }: GroupLocationsMapProps) => {
     );
   }
 
-  return <BaseMapLibreMap geojsonData={mapData} />;
+  return (
+    <BaseMapLibreMap
+      geojsonData={mapData}
+      mapStyleUrl={SATELLITE_STYLE_URL}
+      initialCenter={INITIAL_CENTER}
+      initialZoom={INITIAL_ZOOM}
+    />
+  );
 };
 
 export default GroupLocationsMap;
