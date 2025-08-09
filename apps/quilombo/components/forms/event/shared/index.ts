d64d583a4ef0e52829366c9267f72337b7c2505f
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2025-08-09 18:52:38 +0200

    chore: deleted obsolete event form components
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- apps/quilombo/components/forms/event/shared/index.ts
+++ apps/quilombo/components/forms/event/shared/index.ts
@@ -1,9 +1,4 @@
-export { default as EventDateTimeSection } from './EventDateTimeSection';
-export { default as EventTypeAndUrlSection } from './EventTypeAndUrlSection';
 export { default as EventImageUploadSection } from './EventImageUploadSection';
-export { default as EventLocationSection } from './EventLocationSection';
-
-// New Formik-integrated field components
 export { default as EventDateTimeField } from './EventDateTimeField';
 export { default as EventLocationField } from './EventLocationField';
 export { default as EventTypeField } from './EventTypeField';
