11f0448bbb5185bc1223095f2dd8fd252a14cacd
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2025-11-13 22:35:19 +0100

    feat: added several titles to the DB enum in their appropriate order
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

---
+++ apps/quilombo/db/migrations/0026_concerned_orphan.sql
@@ -0,0 +1,9 @@
+ALTER TYPE "public"."title" ADD VALUE 'graduada' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'graduado' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'formada' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'formado' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'estagiaria' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'estagiario' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'estagianda' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'estagiando' BEFORE 'monitora';--> statement-breakpoint
+ALTER TYPE "public"."title" ADD VALUE 'treinel' BEFORE 'aluna';
