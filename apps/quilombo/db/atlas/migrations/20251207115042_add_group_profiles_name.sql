d1c3874747e0471a81b2963319c1f044aa813b41
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2025-12-07 20:53:20 +0100

    refactor(genealogy): consolidated 5 DB migrations into a single add_genealogy_schema
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- apps/quilombo/db/atlas/migrations/20251207115042_add_group_profiles_name.sql
+++
@@ -1,4 +0,0 @@
--- Modify "group_profiles" table
-ALTER TABLE "genealogy"."group_profiles" ADD COLUMN "name" character varying(255) NOT NULL;
--- Create index "group_profiles_name_idx" to table: "group_profiles"
-CREATE INDEX "group_profiles_name_idx" ON "genealogy"."group_profiles" ("name");
--- apps/quilombo/db/atlas/migrations/20251207192635_update_baptized_to_baptized_by.sql
+++
@@ -1,5 +0,0 @@
--- Update any existing statements using 'baptized' to 'baptized_by'
--- This must be in a separate migration from ADD VALUE due to PostgreSQL limitations.
-UPDATE "genealogy"."statements"
-SET predicate = 'baptized_by'
-WHERE predicate = 'baptized';
